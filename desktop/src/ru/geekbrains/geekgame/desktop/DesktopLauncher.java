d29557bd25f3f324fb3f7dc6bc32642f0c5d0695
v4n0v <avetchinov@gmail.com>
2017-08-16 16:19:45 +0300

    2nd

---
+++ desktop/src/ru/geekbrains/geekgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.geekgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.geekgame.GeekGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GeekGame(), config);
+	}
+}
---
+++ html/src/ru/geekbrains/geekgame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.geekgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import ru.geekbrains.geekgame.GeekGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GeekGame();
+        }
+}
